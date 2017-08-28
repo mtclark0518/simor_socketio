12d6ae0058aeab8787fc086b23d7bff862e3a144
mtclark0518 <mtclark0518@gmail.com>
2017-08-28 14:13:04 -0600

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 
-app.listen(process.env.PORT || 3000
+app.listen(process.env.PORT || 3000);
 
 
 //SERVER
@@ -15,7 +15,7 @@ app.listen(process.env.PORT || 3000, () => {
 //ROUTES
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req,res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + 'index.html');
 });
 
 
